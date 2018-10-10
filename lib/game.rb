de4caf33af3c63de8557dafef470a47b20e80896
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-10 02:49:37 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,6 @@ class Game
     puts "Welcome to Tic-Tac-Toe!!"
     puts "What kind of game would you like to play (0, 1, or 2)?"
     type_of_game = gets.chomp.to_i
-    binding.pry
     if type_of_game != 0
       puts "Who would like to go first and be 'X', Human or Computer?"
       first_player = gets
